78c56569c18007bcc7bf5446dfdf6e025ae79373
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-13 21:09:59 +0545

    Null and undefined values

---
+++ 7_Null_undefined_values/values.js
@@ -0,0 +1,10 @@
+console.clear()
+
+var mVar        // mVar holds no value
+console.log(mVar)
+
+mVar = null     // mVar holds value null
+console.log(mVar)
+
+console.log(undefined == null) 
+console.log(undefined === null) 
