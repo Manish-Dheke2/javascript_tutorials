f2b8e746fd9a6164cd0f261db87018486edbdbda
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-13 22:19:01 +0545

    Conditional Statements

---
+++ 8_conditional_stats/conditional.js
@@ -0,0 +1,38 @@
+console.clear()
+
+// if (5 > 7) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+if (5 > 7) {
+    console.log('5 > 7')
+} else if(7 > 8) {
+    console.log('7 > 8')
+} else {
+    console.log('Nothing')
+}
+
+
+// Take the values given below of length and breadth of a rectangle and check if it is square or not. Change values and see if your code works or not.
+// var length=200; 
+// var breadth=200;
+
+// if (length === breadth) {
+//     console.log("It is a square.")
+// } else {
+//     console.log("It is a rectangle.")
+// }
+
+
+// Write some code to check which number is greatest. Change values and see if your code works or not. 
+// var num1=5, num2=8, num3=20
+
+// if (num1 >= num2 && num1>=num3){
+//     console.log(num1 + " is the greatest")
+// } else if (num2 >= num1 && num2 >= num3) {
+//     console.log(num2 + " is the greatest")
+// } else {
+//     console.log(num3 + " is the greatest")
+// }
