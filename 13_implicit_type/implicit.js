e534a669f64fc2d5cc0b8249934e661d402a0dcc
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-15 20:21:51 +0545

    Implicit types

---
+++ 13_implicit_type/implicit.js
@@ -0,0 +1,33 @@
+console.clear()
+
+// var a = 'Hello'
+// var b = 5
+// var sum = a + b
+
+// console.log(sum)
+// console.log(typeof(sum))
+
+// console.log('hola' + true)
+// console.log(typeof('hola' + true))
+
+// console.log('null' + true)
+// console.log(typeof('null' + true))
+
+// console.log(50 + 'hola')
+// console.log(typeof(50 + 'hola'))
+
+// var mValue = -'30'
+// console.log(mValue)
+// console.log(typeof(mValue))
+
+// if(0) {
+//     console.log('True')
+// } else {
+//     console.log('False')
+// }
+
+if('Rocket' + 1) {
+    console.log('True')
+} else {
+    console.log('False')
+}
