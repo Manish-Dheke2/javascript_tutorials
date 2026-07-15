0ddcf02744429327b65b94f61005f74d387aa5d9
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-15 21:55:25 +0545

    Arithmetic operator

--- 10_arithmetic_operator/arithmetic.js
+++ 10_arithmetic_operator/arithmetic.js
@@ -34,3 +34,15 @@ var num3 = 20
 
 console.log(--num1)
 console.log(num1)
+
+
+// var a = 1;
+// var b = 2;
+// var c;
+// var d;
+// c = ++b;
+// d = a++;
+// c++;
+// b++;
+// ++a;
+// console.log(a + " " + b + " " + c);
