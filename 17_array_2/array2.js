8f7fc9311c8d36e036acd8fe6bc7edd5614eb717
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-23 22:57:29 +0545

    Array part 2 in javascript

---
+++ 17_array_2/array2.js
@@ -0,0 +1,8 @@
+var mFriends = ['Shaw', 'Clara', 'Vab', 'Kun', 'Jacob', 'Dina']
+mFriends.splice(3, 0, 'Donna', 'Rachel')
+
+// 1st tells where to start
+// 2nd tells how many items to be deleted
+// 3rd and 4th and so on tells what items to be added
+console.log(mFriends)
+
