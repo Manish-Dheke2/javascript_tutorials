fe8830f8b70c5d64cbc451456bbde6d4804dc70f
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-25 19:26:14 +0545

    Functions in js

---
+++ 20_functions/function.js
@@ -0,0 +1,20 @@
+// console.clear()
+
+// function mSum() {
+//     var num1 = 10;
+//     var num2 = 20;
+//     var sum = num1 + num2;
+//     console.log(sum)
+// }
+
+// mSum()
+
+function mSum() {
+    var num1 = 10;
+    var num2 = 20;
+    var sum = num1 + num2;
+    return sum
+}
+
+var totalSum = 100 + mSum()
+console.log(totalSum())
