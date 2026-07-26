4c2c30a24aec4c2b48c77f220b475a53489266f6
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-26 23:25:43 +0545

    Scope and environment in javascript

---
+++ 22_scope/scope.js
@@ -0,0 +1,32 @@
+console.clear()
+
+var num1 = 10 // global variable
+
+console.log(num2)
+
+function sum(num2) {
+    console.log(num1)
+    console.log(num1 + num2)
+}
+
+console.log(num1)
+
+
+sum(20)
+
+
+
+
+// var num1 = 10
+
+
+// function sum(num2) {
+//     var total = 0
+//     console.log(total)
+//     total = num1 + num2
+//     console.log(total)
+// }
+
+
+// sum(20)
+// sum(40)
