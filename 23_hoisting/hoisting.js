9f675909f0bf9c741ae25463c2efa38dfc42a493
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-26 23:34:40 +0545

    Hoisting in Javascript

---
+++ 23_hoisting/hoisting.js
@@ -0,0 +1,12 @@
+console.clear()
+
+// Hoisting in Javascript is the mechanism where variables and function can be used before declaring them 
+
+num1 = 10
+console.log(num1)
+var num1
+
+str1 = "Demo String"
+console.log(str1)
+var str1
+
