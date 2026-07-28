baedacd99019e4950ca9eb7c58353bb9623a6a70
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-28 22:34:28 +0545

    Select HTML elements in JS

---
+++ 26_select_html_elements/select.js
@@ -0,0 +1,10 @@
+console.log("Script loaded")
+
+console.log(document.getElementById('topbar'))
+
+console.log(document.getElementsByClassName("myClass"))
+
+let tags = document.getElementsByTagName("p")
+console.log(tags)
+
+
